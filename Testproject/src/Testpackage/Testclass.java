e49e93cf258aa05fe3c9ba4415d192cc75928188
843889 <843889@01HW451901.India.TCS.com>
2016-06-13 11:04:12 +0530

    Change in test class

--- Testproject/src/Testpackage/Testclass.java
+++ Testproject/src/Testpackage/Testclass.java
@@ -5,6 +5,8 @@ public class Testclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Test change 1 ");
+System.out.println("Test change 2 ");
+
 	}
 
 }
